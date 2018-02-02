72f2e456e59c5dd58837ebcd9883129601208be6
edmond120 <edmondwong888@msn.com>
2018-02-02 15:28:41 -0500

    started, language is python

---
+++ generate.py
@@ -0,0 +1,12 @@
+import random
+
+def header(file, file_type, width, height, max_color_value):
+	file
+
+def main():
+	random.seed()
+	file = open('image.ppm','w')
+	header(file,"P3",4,3,255)
+
+if __name__ == '__main__':
+	main()
